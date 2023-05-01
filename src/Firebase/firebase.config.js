93621459b6bbe7b7d5cb01acd39036bb99b620f7
Sifatulla21 <sifat2109@gmail.com>
2023-05-01 23:34:44 +0600

    hide keys

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log("Env", import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyBuUJPHtHXup9Vl3Ls0e7CocsJ89Un0O_o",
-  authDomain: "the-news-dragon-4461e.firebaseapp.com",
-  projectId: "the-news-dragon-4461e",
-  storageBucket: "the-news-dragon-4461e.appspot.com",
-  messagingSenderId: "291929062477",
-  appId: "1:291929062477:web:d7467f5946bda031c13004"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
